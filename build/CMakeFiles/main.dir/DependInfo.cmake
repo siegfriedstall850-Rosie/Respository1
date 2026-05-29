b198e9c3959fd0932bd3213eb538b5515b94cf65
Rosie <3433193602@qq.com>
2026-05-29 20:22:07 +0800

    首次提交：上传项目初始代码

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/code/test_5_27/src/main.c" "CMakeFiles/main.dir/src/main.c.obj" "gcc" "CMakeFiles/main.dir/src/main.c.obj.d"
+  "D:/code/test_5_27/src/print.c" "CMakeFiles/main.dir/src/print.c.obj" "gcc" "CMakeFiles/main.dir/src/print.c.obj.d"
+  "D:/code/test_5_27/src/print1.c" "CMakeFiles/main.dir/src/print1.c.obj" "gcc" "CMakeFiles/main.dir/src/print1.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.c.obj"
+  "CMakeFiles/main.dir/src/main.c.obj.d"
+  "CMakeFiles/main.dir/src/print.c.obj"
+  "CMakeFiles/main.dir/src/print.c.obj.d"
+  "CMakeFiles/main.dir/src/print1.c.obj"
+  "CMakeFiles/main.dir/src/print1.c.obj.d"
+  "D:/code/test_5_27/bin/libmain.dll.a"
+  "D:/code/test_5_27/bin/main.exe"
+  "D:/code/test_5_27/bin/main.exe.manifest"
+  "D:/code/test_5_27/bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
